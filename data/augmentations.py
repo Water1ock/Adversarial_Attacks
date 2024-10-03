610fb3350373110b6cda080a8e15fac3d9d32be5
Water1ock <varmashubhanjay@gmail.com>
2024-10-03 19:22:20 +0100

    Modified augmentations to return an iterable rather than a Composed

--- data/augmentations.py
+++ data/augmentations.py
@@ -2,14 +2,14 @@ import torchvision.transforms as transforms
 
 def get_augmentations():
     """
-    Returns a composition of transformations for data augmentation.
+    Returns a list of transformations for data augmentation.
 
     Returns:
-    - transform: A composed transformation for augmenting training images.
+    - transform: A list of transformations for augmenting training images.
     """
-    return transforms.Compose([
+    return [
         transforms.RandomHorizontalFlip(),  # Randomly flip images horizontally
         transforms.RandomCrop(32, padding=4),  # Randomly crop with padding
         transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # Random color jitter
         transforms.ToTensor(),  # Convert PIL Image to tensor
-    ])
+    ]
