e2f1ef68dc060cbdb936f45a5f1c15b76c7e7e83
Water1ock <varmashubhanjay@gmail.com>
2024-10-03 19:18:58 +0100

    main.py change

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from experiments.experiment_resnet import main as resnet_experiment
 
 def run_resnet_experiment():
@@ -9,14 +10,23 @@ def run_resnet_experiment():
 
     # Create a dummy argument list
     args = [
-        'main.py',  # Script name
         '--num_epochs', str(num_epochs),
         '--learning_rate', str(learning_rate),
         '--batch_size', str(batch_size),
         '--epsilon', str(epsilon)
     ]
 
-    resnet_experiment(args)
+    # Parse the arguments to create a Namespace object
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_epochs', type=int, required=True)
+    parser.add_argument('--learning_rate', type=float, required=True)
+    parser.add_argument('--batch_size', type=int, required=True)
+    parser.add_argument('--epsilon', type=float, required=True)
+
+    parsed_args = parser.parse_args(args)
+
+    # Call the experiment function with the parsed arguments
+    resnet_experiment(parsed_args)
 
 if __name__ == "__main__":
     run_resnet_experiment()
